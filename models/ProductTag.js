bbda94f7222386514ed762ab75d725ac37311474
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-03 18:38:58 -0700

    defined columns in category, product, tag and product tag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,38 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+     // reference to Product model
+     model: Product,
+
+     // column name of the referenced model
+     key: 'id',
+
+     // This declares when to check the foreign key constraint. PostgreSQL only.
+     //deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+       references: {
+     // reference to Tag model
+     model: Tag,
+
+     // column name of the referenced model
+     key: 'id',
+
+     // This declares when to check the foreign key constraint. PostgreSQL only.
+     //deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    }
   },
   {
     sequelize,
