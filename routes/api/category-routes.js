39cda423dc33fbf5a86c1dc8a0e6a66ab9411491
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-03 21:44:38 -0700

    Added async/await to fix error to GET api categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,10 +3,10 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   try {
-    const data = Category.findAll({
+    const data = await Category.findAll({
     include: [{
       model: Product,
       attributes: [
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
     }]
     });
     if (!data) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'ERROR 404' });
       return;
     }
     res.status(200).json(data);
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
     res.status(500).json(err);
   }
 }); 
-  // be sure to include its associated Products
+// be sure to include its associated Products
 
 
 router.get('/:id', (req, res) => {
