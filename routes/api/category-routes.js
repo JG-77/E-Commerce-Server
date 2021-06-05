df3a896586621fe9099a9944f38ecea88b1c96b3
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-04 18:06:31 -0700

    set up api POST endpoint to create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,9 +37,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
-
+// creates a new category
+router.post('/', async (req, res) => {
+  try {
+    const createdData = await Category.create(req.body);
+    res.status(200).json(createdData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
