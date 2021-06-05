c66904582dafb1475667443a5c2e83c52c07fe7c
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-04 18:36:17 -0700

    Have all api endpoints set up for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-// Finds all categories and includes associated products
+// finds all categories and includes associated products
 router.get('/', async (req, res) => {
   try {
     const data = await Category.findAll({
@@ -47,12 +47,44 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// updates a category by its `id` value
+router.put('/:id', async (req, res) => {
+    try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// deletes a category by its `id` value
+router.delete('/:id', async (req, res) => {
+    try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
