77b8104a06e55e7a48de32a3c152c4b668b9d3ce
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-06 15:19:14 -0700

    set up api/products route for GET methods by product id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,8 +3,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
-// be sure to include its associated Category and Tag data
+// get all products and include its associated Category and Tag data
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
@@ -20,10 +19,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// get one product by its id and include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with this id' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
