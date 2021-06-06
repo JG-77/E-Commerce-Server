d8cb10485010b5da2100129399d28d463df73f7c
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-06-06 16:17:42 -0700

    Set up PUT method for updating tag api route

--- models/Tag.js
+++ models/Tag.js
@@ -13,10 +13,9 @@ Tag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-      tag_name: {
-        type: DataTypes.STRING,
-      }
-
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,12 +35,34 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+// update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
